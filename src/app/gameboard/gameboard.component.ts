2a8ac108c4dfb9694d242dd54a727251754ad743
jinyuan <jinyuan@users.noreply.github.com>
2021-04-03 12:08:28 +0800

    Feature/angular upgrade (#1)
    * upgrade to angular 11
    
    * upgrade deps, fix vul
    
    * patch github actions

--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -4,10 +4,10 @@ import {IdiomsService} from "../providers/idioms.service";
 import {interval, Subscription} from "rxjs";
 import {GamePiece} from "../models/GamePiece";
 import {Difficulty} from "../models/Difficulty";
-import {faClipboard, faInfo, faInfoCircle, faPlay, faStopwatch, faSyncAlt} from "@fortawesome/free-solid-svg-icons";
+import {faClipboard, faInfoCircle, faPlay, faStopwatch, faSyncAlt} from "@fortawesome/free-solid-svg-icons";
 import {FaIconLibrary} from "@fortawesome/angular-fontawesome";
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {BsModalService, BsModalRef} from 'ngx-bootstrap/modal';
 import {ActivatedRoute} from "@angular/router";
 import {TranslateService} from "@ngx-translate/core";
 
@@ -27,10 +27,10 @@ export class GameboardComponent implements OnInit, AfterViewInit{
     library.addIcons(
       faStopwatch,
       faClipboard,
-      faGithub,
       faPlay,
       faSyncAlt,
       faInfoCircle,
+      faGithub,
     );
     this.selectedLang = translate.getBrowserLang();
     translate.setDefaultLang(this.selectedLang);
